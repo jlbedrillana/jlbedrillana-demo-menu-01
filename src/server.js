79988ddfe12ce1aaa1c5115ce2bc90b6106fa080
jlbedrillana <jlbedrillana@gmail.com>
2021-10-03 06:34:14 -0500

    Heroku update code 04

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-menu'));
+app.use(express.static(__dirname+'/dist/app-menu'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-menu/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/app-menu/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
